9895f56c4ee598fcfdc66fa525988dc995ac88f4
Sree Grandhe <sreegrandhe@gmail.com>
2020-04-06 18:32:01 -0500

    Change Angular Fire Version For Auth

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,14 +4,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
+import {BarcodeScanner} from '@ionic-native/barcode-scanner/ngx';
+
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab1Page }])
+    RouterModule.forChild([{ path: '', component: Tab1Page }]),
   ],
+  providers: [BarcodeScanner],
   declarations: [Tab1Page]
 })
-export class Tab1PageModule {}
+export class Tab1PageModule {
+
+}
