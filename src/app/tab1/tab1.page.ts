4a8e4860e9c09d8430a05c4ade5e86757f43f1af
Sree Grandhe <sreegrandhe@gmail.com>
2020-04-07 19:59:20 -0500

    Android Build First Commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import {Component, ViewChild, ElementRef} from '@angular/core';
 import {ToastController, LoadingController, Platform} from '@ionic/angular';
 import {BarcodeScanner,BarcodeScannerOptions} from '@ionic-native/barcode-scanner/ngx';
+import jsQR from 'jsqr';
 
 @Component({
     selector: 'app-tab1',
@@ -8,8 +9,18 @@ import {BarcodeScanner,BarcodeScannerOptions} from '@ionic-native/barcode-scanne
     styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-   constructor(public barcodeScanner: BarcodeScanner) {
-   }
+    // https://devdactic.com/pwa-qr-scanner-ionic/
+
+    @ViewChild('fileinput', { static: false }) fileinput: ElementRef;
+    @ViewChild('canvas', { static: false }) canvas: ElementRef;
+
+    canvasElement: any;
+    videoElement: any;
+    canvasContext: any;
+    scanResult = null;
+
+    constructor(public barcodeScanner: BarcodeScanner,  private toastCtrl: ToastController) {
+    }
 
    scan(){
        // Function doesn't work on web version
@@ -19,4 +30,47 @@ export class Tab1Page {
            console.log('Error', err);
        });
    }
+
+    captureImage() {
+        this.fileinput.nativeElement.click();
+    }
+
+    handleFile(files: FileList) {
+        const file = files.item(0);
+        const img = new Image();
+        img.onload = () => {
+            this.canvasContext.drawImage(img, 0, 0, this.canvasElement.width, this.canvasElement.height);
+            const imageData = this.canvasContext.getImageData(
+                0,
+                0,
+                this.canvasElement.width,
+                this.canvasElement.height
+            );
+            const code = jsQR(imageData.data, imageData.width, imageData.height, {
+                inversionAttempts: 'dontInvert'
+            });
+
+            if (code) {
+                this.scanResult = code.data;
+                this.showQrToast();
+            }
+        };
+        img.src = URL.createObjectURL(file);
+    }
+    async showQrToast() {
+        const toast = await this.toastCtrl.create({
+            message: `Open ${this.scanResult}?`,
+            position: 'top',
+            buttons: [
+                {
+                    text: 'Open',
+                    handler: () => {
+                        window.open(this.scanResult, '_system', 'location=yes');
+                    }
+                }
+            ]
+        });
+        toast.present();
+    }
+
 }
