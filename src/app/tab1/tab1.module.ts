ef66d508e9963e908fc809ef3276973677481fba
Sree Grandhe <sreegrandhe@live.com>
2020-07-20 23:55:32 -0500

    Create front-end for Algorithm output

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -5,12 +5,14 @@ import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {Tab1Page} from './tab1.page';
 import {AngularFireAuthModule} from '@angular/fire/auth';
+import {AngularFireFunctionsModule} from '@angular/fire/functions';
 
 @NgModule({
     imports: [
         IonicModule,
         CommonModule,
         AngularFireAuthModule,
+        AngularFireFunctionsModule,
         FormsModule,
         RouterModule.forChild([{path: '', component: Tab1Page}]),
     ],
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,15 +1,57 @@
 import {Component} from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {AngularFireStorage} from '@angular/fire/storage';
+import {RequestsProgramService} from '../requests-program.service';
+import { AngularFireFunctions } from '@angular/fire/functions';
 
 @Component({
     selector: 'app-tab1',
     templateUrl: 'tab1.page.html',
     styleUrls: ['tab1.page.scss'],
-    providers: []
+    providers: [AngularFirestore, AngularFireStorage]
 })
 export class Tab1Page {
+    suggestions: Array<any>;
 
-    constructor() {
-
+    constructor(private fns: AngularFireFunctions,public rs: RequestsProgramService, private firestore: AngularFirestore,
+                private storage: AngularFireStorage) {
+            this.suggestions = [];
+            this.runSuggestions();
     }
 
+    renderSuggestions(val: Array<any>) {
+        this.suggestions = [];
+        val.forEach(info => {
+            this.firestore.collection('users').doc(info.id).get().subscribe(userDoc => {
+                const suggestionObject = {
+                    id: userDoc.id,
+                    name: userDoc.get('name'),
+                    bio: userDoc.get('bio'),
+                    img: '',
+                    percentage: info.percentage
+                };
+                if (userDoc.get('profilepic').startsWith('h')) {
+                    suggestionObject.img = userDoc.get('profilepic');
+                } else {
+                    this.storage.storage.refFromURL(userDoc.get('profilepic')).getDownloadURL().then(url => {
+                        suggestionObject.img = url;
+                    });
+                }
+                this.suggestions.push(suggestionObject);
+            });
+        });
+    }
+
+    runSuggestions(){
+        const algoFun = this.fns.httpsCallable('suggestionsAlgorithm');
+        algoFun({variable: null}).subscribe(userIdArray => {
+            // Array of uid's and percentages
+            this.renderSuggestions(userIdArray);
+        });
+    }
+
+    doRefresh(event) {
+        this.runSuggestions();
+        event.target.complete();
+    }
 }
