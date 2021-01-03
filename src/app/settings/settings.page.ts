63671c1013fe4e29284c3d38bcc0d429f2a63795
Sree Grandhe <sreegrandhe@live.com>
2021-01-02 21:09:03 -0600

    Create Update-Profile Service

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -4,12 +4,13 @@ import {AngularFireAuth} from '@angular/fire/auth';
 import {Router} from '@angular/router';
 import {AngularFirestore, AngularFirestoreDocument} from '@angular/fire/firestore';
 import {AngularFireStorage} from '@angular/fire/storage';
+import {UpdateProfileService} from '../services/update-profile.service';
 
 @Component({
     selector: 'app-settings',
     templateUrl: './settings.page.html',
     styleUrls: ['./settings.page.scss'],
-    providers: [AngularFireAuth, AngularFireStorage]
+    providers: [AngularFireAuth, UpdateProfileService, AngularFireStorage]
 })
 
 
@@ -20,15 +21,11 @@ export class SettingsPage implements OnInit {
     currentUserBasic: any;
     currentUser: any;
     currentUserId: string;
-    fileName: string;
-    latestPhoto: string | ArrayBuffer;
     myInterests: Array<string>;
     myValues: Array<string>;
 
     constructor(private modalCtrl: ModalController, private auth: AngularFireAuth, private r: Router, private firestore: AngularFirestore
-        , private toastController: ToastController, private storage: AngularFireStorage) {
-        this.fileName = null;
-        this.latestPhoto = null;
+        , private toastController: ToastController, private storage: AngularFireStorage, private updateProfileService: UpdateProfileService) {
         this.currentUserId = this.auth.auth.currentUser.uid;
         this.currentUserRef = this.firestore.collection('users').doc(this.currentUserId);
     }
@@ -55,59 +52,7 @@ export class SettingsPage implements OnInit {
     }
 
     updateSettings() {
-        if ((document.getElementById('username') as HTMLInputElement).value === '' ||
-            (document.getElementById('bio') as HTMLInputElement).value === '' ||
-            this.myInterests === undefined || this.myValues === undefined) {
-            this.presentToast('Whoops! Looks like some of your settings might be empty');
-        } else if(this.myInterests.length > 5|| this.myValues.length > 5){
-            this.presentToast('Whoops! Looks like you entered more than 5 interest/values');
-        } else {
-            if (this.fileName != null) {
-                // TODO delete old profile pic
-                const ref = this.storage.ref(this.currentUserId + this.fileName);
-                if (typeof this.latestPhoto === 'string') {
-                    ref.putString(this.latestPhoto, 'data_url').then(snapshot => {
-                        this.currentUserRef.update({
-                            name: (document.getElementById('username') as HTMLInputElement).value,
-                            bio: (document.getElementById('bio') as HTMLInputElement).value,
-                            profilepic: 'gs://circles-4d081.appspot.com/' + (
-                                this.currentUserId
-                                + this.fileName)
-                        }).then(value => {
-                            this.fileName = null;
-                            this.latestPhoto = null;
-                        })
-                    });
-                }
-            }
-            this.firestore.collection('socials').doc(this.currentUserId).update({
-                facebookID: (document.getElementById('fb') as HTMLInputElement).value,
-                instagramID: (document.getElementById('ig') as HTMLInputElement).value,
-                twitterID: (document.getElementById('tw') as HTMLInputElement).value,
-                personalEmailID: (document.getElementById('peem') as HTMLInputElement).value,
-                linkedinID: (document.getElementById('li') as HTMLInputElement).value,
-                professionalEmailID: (document.getElementById('prem') as HTMLInputElement).value,
-                snapchatID: (document.getElementById('sc') as HTMLInputElement).value,
-                tiktokID: (document.getElementById('tt') as HTMLInputElement).value,
-                venmoID: (document.getElementById('ve') as HTMLInputElement).value,
-                websiteID: (document.getElementById('ws') as HTMLInputElement).value,
-            });
-            this.firestore.collection('preferences').doc(this.currentUserId).update({
-                preferences: this.myInterests,
-                valueTraits: this.myValues,
-            }).then(val => {
-                this.presentToast('Settings Updated!');
-            })
-        }
-
-    }
-
-    async presentToast(displayMessage: string) {
-        const toast = await this.toastController.create({
-            message: displayMessage,
-            duration: 2000
-        });
-        toast.present();
+        this.updateProfileService.updateSettings(this.myInterests, this.myValues);
     }
 
     logout() {
@@ -128,17 +73,7 @@ export class SettingsPage implements OnInit {
     }
 
     handleFile(files: FileList) {
-        const file = files.item(0);
-
-        const reader = new FileReader();
-        reader.onload = (e) => {
-            document.getElementById('imagePreview').style.backgroundImage = 'url(' + e.target.result + ')';
-        }
-        this.fileName = file.name;
-        reader.readAsDataURL(file);
-        reader.addEventListener('loadend', () => {
-            this.latestPhoto = reader.result;
-        });
+        this.updateProfileService.handleFile(files);
     }
 
 }
