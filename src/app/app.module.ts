a4063f95a09e80a4cd4c97ca70cd456f00a37017
Sree Grandhe <sreegrandhe@live.com>
2021-06-05 17:03:04 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,15 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouteReuseStrategy} from '@angular/router';
 import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
-import {SplashScreen} from '@ionic-native/splash-screen/ngx';
-import {StatusBar} from '@ionic-native/status-bar/ngx';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {AngularFireModule} from '@angular/fire';
 import {environment} from '../environments/environment';
 import {AngularFirestore} from '@angular/fire/firestore';
-import {FCM} from '@ionic-native/fcm/ngx';
 import {ORIGIN as FUNCTIONS_ORIGIN} from '@angular/fire/functions';
 import { URL as DATABASE_URL } from '@angular/fire/database';
 import { SETTINGS as FIRESTORE_SETTINGS } from '@angular/fire/firestore';
+import {FCM} from '@capacitor-community/fcm';
 
 @NgModule({
     declarations: [AppComponent],
