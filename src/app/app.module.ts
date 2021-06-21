107440914ce903065c4d2d6264265177c4662491
Sree Grandhe <sreegrandhe@live.com>
2021-06-21 00:10:00 -0500

    Implemented Service to Primary App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,35 +11,45 @@ import {ORIGIN as FUNCTIONS_ORIGIN} from '@angular/fire/functions';
 import { URL as DATABASE_URL } from '@angular/fire/database';
 import { SETTINGS as FIRESTORE_SETTINGS } from '@angular/fire/firestore';
 import {FCM} from '@capacitor-community/fcm';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { AuthHandler } from './services/authHandler.service';
 
 @NgModule({
     declarations: [AppComponent],
     entryComponents: [],
-    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, AngularFireModule.initializeApp(environment.firebase)],
+    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, 
+        HttpClientModule, AngularFireModule.initializeApp(environment.firebase)],
     providers: [
         AngularFirestore,
+        AngularFireStorage,
         FCM,
+        AuthHandler,
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthHandler,
+            multi: true
+        },
         {provide: RouteReuseStrategy, useClass: IonicRouteStrategy,
         },
        // TODO Remove in production
-        {
-            provide: FIRESTORE_SETTINGS,
-            useValue: environment.production ? undefined : {
-                host: 'localhost:8070',
-                ssl: false
-            }
-        },
-        {
-            provide: FUNCTIONS_ORIGIN,
-            useValue: environment.production ? undefined : {
-                host: 'localhost:5000',
-            }
-        },
-        {
-            provide: DATABASE_URL,
-            useValue: !environment.production ? `http://localhost:9000?ns=${environment.firebase.projectId}` : undefined
-        }
+        // {
+        //     provide: FIRESTORE_SETTINGS,
+        //     useValue: environment.production ? undefined : {
+        //         host: 'localhost:8080',
+        //         ssl: false
+        //     }
+        // },
+        // {
+        //     provide: FUNCTIONS_ORIGIN,
+        //     useValue: environment.production ? undefined : {
+        //         host: 'localhost:5001',
+        //     }
+        // },
+        // {
+        //     provide: DATABASE_URL,
+        //     useValue: !environment.production ? `http://localhost:9000?ns=${environment.firebase.projectId}` : undefined
+        // }
     ],
     bootstrap: [AppComponent],
 })
