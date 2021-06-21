9643fc17943cad504d0ae915b1e9c467579f1054
Sree Grandhe <sreegrandhe@live.com>
2021-06-21 00:12:45 -0500

    Implemented Services to Pings

--- src/app/circledash/new-ping/new-ping.module.ts
+++ src/app/circledash/new-ping/new-ping.module.ts
@@ -7,14 +7,12 @@ import {IonicModule} from '@ionic/angular';
 import {NewPingPageRoutingModule} from './new-ping-routing.module';
 
 import {NewPingPage} from './new-ping.page';
-import {AngularFireStorageModule} from '@angular/fire/storage';
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
         IonicModule,
-        AngularFireStorageModule,
         NewPingPageRoutingModule
     ],
     // declarations: [NewPingPage]
