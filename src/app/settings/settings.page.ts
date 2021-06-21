b451de85302b2489867fb7e18a17df2ae14f320d
Sree Grandhe <sreegrandhe@live.com>
2021-06-21 00:13:13 -0500

    Implemented Services to Settings

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,18 +1,18 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ModalController, ToastController} from '@ionic/angular';
+import {ModalController} from '@ionic/angular';
 import {AngularFireAuth} from '@angular/fire/auth';
 import {Router} from '@angular/router';
 import {AngularFirestore, AngularFirestoreDocument} from '@angular/fire/firestore';
-import {AngularFireStorage} from '@angular/fire/storage';
-import {UpdateProfileService} from '../services/update-profile.service';
+import {UsersService} from '../services/users.service';
 import {map} from 'rxjs/operators';
 import {Observable} from 'rxjs';
+import {UtilsService} from '../services/utils.service'
 
 @Component({
     selector: 'app-settings',
     templateUrl: './settings.page.html',
     styleUrls: ['./settings.page.scss'],
-    providers: [AngularFireAuth, UpdateProfileService, AngularFireStorage]
+    providers: [AngularFireAuth, UsersService]
 })
 
 
@@ -25,7 +25,7 @@ export class SettingsPage implements OnInit {
     currentUserSocials: Observable<any>;
 
     constructor(private modalCtrl: ModalController, private auth: AngularFireAuth, private r: Router, private firestore: AngularFirestore
-        , private toastController: ToastController, private storage: AngularFireStorage, private updateProfileService: UpdateProfileService) {
+        ,private us: UsersService, private utils: UtilsService) {
         this.currentUserId = this.auth.auth.currentUser.uid;
         this.currentUserRef = this.firestore.collection('users').doc(this.currentUserId);
         this.currentUserBasic = new Observable<any>();
@@ -33,29 +33,24 @@ export class SettingsPage implements OnInit {
     }
 
     ngOnInit() {
-        this.currentUserBasic = this.currentUserRef.get().pipe(map(ret => {
-            this.setData(ret.get('profilepic'));
+        this.currentUserBasic = this.us.getUserBasic(this.auth.auth.currentUser.uid).pipe(map((ret:any) => {
+            document.getElementById('imagePreview').style.backgroundImage = 'url(' + ret.data.profilepic + ')';
             return {
-                name: ret.get('name'),
-                bio: ret.get('bio')
+                name: ret.data.name,
+                bio: ret.data.bio
             }
         }))
 
         this.currentUserSocials = this.firestore.collection('socials').doc(this.currentUserId).get().pipe(map(ret => ret.data()));
     }
 
-    setData(res){
-        if (res.startsWith('h')) {
-            document.getElementById('imagePreview').style.backgroundImage = 'url(' + res + ')';
-        } else {
-            this.storage.storage.refFromURL(res).getDownloadURL().then(url => {
-                document.getElementById('imagePreview').style.backgroundImage = 'url(' + url + ')';
-            });
-        }
-    }
-
     updateSettings() {
-        this.updateProfileService.updateSettings();
+        this.us.updateProfile().subscribe(val => {
+            this.utils.presentToast('Profile Updated!');
+        }, err => {
+            this.utils.presentToast('Whoops! Update Failed');
+            console.error(err.error);
+        });
     }
 
     logout() {
@@ -76,7 +71,7 @@ export class SettingsPage implements OnInit {
     }
 
     handleFile(files: FileList) {
-        this.updateProfileService.handleFile(files);
+        this.us.handleFile(files);
     }
 
 }
