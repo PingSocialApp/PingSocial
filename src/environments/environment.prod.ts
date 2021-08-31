90e32b8bc947525a1a0a64ab0cc3aa8143723022
Sree Grandhe <sreegrandhe@live.com>
2021-08-31 13:26:14 -0500

    Beta Production Environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,27 @@
 export const environment = {
-    production: true
+    production: true,
+    firebase:
+    {
+        apiKey: 'AIzaSyAcR-hQL00RZ7nlSH_Ry80lxcLdCGhIRao',
+        authDomain: 'circles-4d081.firebaseapp.com',
+        databaseURL: 'https://circles-4d081.firebaseio.com',
+        projectId: 'circles-4d081',
+        storageBucket: 'circles-4d081.appspot.com',
+        messagingSenderId: '1002096850890',
+        appId: '1:1002096850890:web:2d74c84b63048faaccc133',
+        measurementId: 'G-LE1HC0KW0F'
+    },
+    mapbox: {
+        style: 'mapbox://styles/sreegrandhe/ckak2ig0j0u9v1ipcgyh9916y?optimize=true',
+        accessToken: 'pk.eyJ1Ijoic3JlZWdyYW5kaGUiLCJhIjoiY2thanhpaDByMDBxZDJybGdxMnQ5ZnNxaiJ9.mkcuMoDmiPapnplpJ_lgaQ'
+    },
+    apiURL: {
+        default: 'https://ping-dev.herokuapp.com/api/v1/',
+        events: 'https://ping-dev.herokuapp.com/api/v1/events/',
+        users:'https://ping-dev.herokuapp.com/api/v1/users/',
+        geoPing:'https://ping-dev.herokuapp.com/api/v1/geoping/',
+        links:'https://ping-dev.herokuapp.com/api/v1/links/',
+        requests:'https://ping-dev.herokuapp.com/api/v1/requests/',
+        markers: 'https://ping-dev.herokuapp.com/api/v1/markers/'
+    }
 };
