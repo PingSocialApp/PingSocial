60cf527cdaae73369427162260516565d6cd72a2
Sree Grandhe <sreegrandhe@live.com>
2021-09-02 16:03:19 -0500

    Update environment formatting

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,17 +4,16 @@
 
 export const environment = {
     production: false,
-    firebase:
-        {
-            apiKey: 'AIzaSyAcR-hQL00RZ7nlSH_Ry80lxcLdCGhIRao',
-            authDomain: 'circles-4d081.firebaseapp.com',
-            databaseURL: 'https://circles-4d081.firebaseio.com',
-            projectId: 'circles-4d081',
-            storageBucket: 'circles-4d081.appspot.com',
-            messagingSenderId: '1002096850890',
-            appId: '1:1002096850890:web:2d74c84b63048faaccc133',
-            measurementId: 'G-LE1HC0KW0F'
-        },
+    firebase: {
+        apiKey: 'AIzaSyAcR-hQL00RZ7nlSH_Ry80lxcLdCGhIRao',
+        authDomain: 'circles-4d081.firebaseapp.com',
+        databaseURL: 'https://circles-4d081.firebaseio.com',
+        projectId: 'circles-4d081',
+        storageBucket: 'circles-4d081.appspot.com',
+        messagingSenderId: '1002096850890',
+        appId: '1:1002096850890:web:2d74c84b63048faaccc133',
+        measurementId: 'G-LE1HC0KW0F'
+    },
     mapbox: {
         style: 'mapbox://styles/sreegrandhe/ckak2ig0j0u9v1ipcgyh9916y?optimize=true',
         accessToken: 'pk.eyJ1Ijoic3JlZWdyYW5kaGUiLCJhIjoiY2thanhpaDByMDBxZDJybGdxMnQ5ZnNxaiJ9.mkcuMoDmiPapnplpJ_lgaQ'
