a5ab8d5324366b7d466a1dc336149c60aa101da9
Neeley Pate <neeley.pate@gmail.com>
2021-09-21 12:02:23 -0500

    basic tutorial implemented

--- src/app/login/registration/tutorial/tutorial.page.ts
+++ src/app/login/registration/tutorial/tutorial.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-tutorial',
@@ -7,13 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TutorialPage implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
     console.log("we in the tutorial!!");
     console.log(this);
     //TODO: look at how to pass to navigate['']
     //Promise.all(this.r.navigate(['']));
+  }
+
+  endTutorial(){
+    console.log(this.router);
+    this.router.navigate(['']);
+    console.log("ended");
   }
 
 }
